719069b5025fbc1ac852c5c4ea7c739d3287cca3
ReDay Zarra <redayzarra@gmail.com>
2023-10-08 11:45:05 -0400

    Setting up homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-red-600'>Hello</h1>
+    <Button>click me</Button>
   )
 }
