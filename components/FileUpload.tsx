e1feb045723ad2799c465296eb58d0045ef7d456
ReDay Zarra <redayzarra@gmail.com>
2023-10-13 10:19:49 -0400

    Adding loading and mutations to FileUpload component

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -3,13 +3,15 @@
 import { uploadToS3 } from "@/lib/s3";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { InboxIcon } from "lucide-react";
-import React from "react";
+import { InboxIcon, Loader2 } from "lucide-react";
+import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import toast from "react-hot-toast";
 
 const FileUpload = () => {
-  const { mutate } = useMutation({
+  const [uploading, setUploading] = useState(false);
+
+  const { mutate, isLoading } = useMutation({
     mutationFn: async ({
       file_key,
       file_name,
@@ -30,7 +32,6 @@ const FileUpload = () => {
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
     onDrop: async (acceptedFiles) => {
-      console.log(acceptedFiles);
       const file = acceptedFiles[0];
 
       if (file.size > 10 * 1024 * 1024) {
@@ -39,6 +40,8 @@ const FileUpload = () => {
       }
 
       try {
+        setUploading(true);
+
         const data = await uploadToS3(file);
 
         if (!data?.file_key || !data.file_name) {
@@ -58,6 +61,8 @@ const FileUpload = () => {
         console.log("data", data);
       } catch (error) {
         console.log(error);
+      } finally {
+        setUploading(false);
       }
     },
   });
@@ -71,10 +76,18 @@ const FileUpload = () => {
         })}
       >
         <input {...getInputProps} />
-        <>
-          <InboxIcon className="w-10 h-10 text-blue-500" />
-          <p className="mt-2 text-sm text-slate-400">Drop your PDF here!</p>
-        </>
+        {uploading || isLoading ? (
+          <>
+            // Loading state
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">Analyzing with GPT...</p>
+          </>
+        ) : (
+          <>
+            <InboxIcon className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Drop your PDF here!</p>
+          </>
+        )}
       </div>
     </div>
   );
