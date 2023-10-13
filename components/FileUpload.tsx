19dc0a93bbae2cc0591f33e47ff9aece9de40544
ReDay Zarra <redayzarra@gmail.com>
2023-10-12 20:15:17 -0400

    Using Toast to show error messages

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,11 +1,31 @@
 "use client";
 
 import { uploadToS3 } from "@/lib/s3";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 import { InboxIcon } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
+import toast from "react-hot-toast";
 
 const FileUpload = () => {
+  const { mutate } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/create-chat", {
+        file_key,
+        file_name,
+      });
+
+      return response.data;
+    },
+  });
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
@@ -14,12 +34,27 @@ const FileUpload = () => {
       const file = acceptedFiles[0];
 
       if (file.size > 10 * 1024 * 1024) {
-        alert("Please return a smaller file.");
+        toast.error("File too large!");
         return;
       }
 
       try {
         const data = await uploadToS3(file);
+
+        if (!data?.file_key || !data.file_name) {
+          toast.error("Soemthing is wrong!");
+          return;
+        }
+
+        mutate(data, {
+          onSuccess: (data) => {
+            console.log(data);
+          },
+          onError: (err) => {
+            toast.error("Error creating chat.");
+          },
+        });
+
         console.log("data", data);
       } catch (error) {
         console.log(error);
