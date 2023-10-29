10ee0954cfd413da97b62b8c805f5ff4354e38ee
ReDay Zarra <redayzarra@gmail.com>
2023-10-29 10:49:41 -0400

    Restarting project with the goal to learn

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,8 @@
-import FileUpload from "@/components/FileUpload";
-import { Button } from "@/components/ui/button";
-import { UserButton, auth } from "@clerk/nextjs";
-import { LogIn } from "lucide-react";
-import Link from "next/link";
 
-export default async function Home() {
-  const { userId } = await auth();
-  const isAuth = !!userId;
-
+export default function Home() {
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col items-center text-center">
-          <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
-            <UserButton afterSignOutUrl="/" />
-          </div>
-          <div className="flex mt-2">
-            {isAuth && <Button>Go to Chats</Button>}
-          </div>
-          <p className="max-w-xl mt-1 text-lg text-slate-600">
-            Join thousands of students who work harder than you.
-          </p>
-          <div className="w-full mt-4">
-            {isAuth ? (
-              <FileUpload />
-            ) : (
-              <Link href={"/sign-in"}>
-                <Button>
-                  Login to get Started! <LogIn className="w-4 h-4 ml-2" />
-                </Button>
-              </Link>
-            )}
-          </div>
-        </div>
-      </div>
+    <div className="">
+      Hello World
     </div>
-  );
+  )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function convertToAscii(inputString: string) {
-  // remove non ascii characters
-  const asciiString = inputString.replace(/[^\x00-\x7F]+/g, "");
-  return asciiString;
+  return twMerge(clsx(inputs))
 }
