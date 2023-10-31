16f34b26b44b72f0004c9e0df4da678a9a562d40
ReDay Zarra <redayzarra@gmail.com>
2023-10-30 23:40:32 -0400

    Setting up Clerk for authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="">
-      Hello World
-    </div>
+    <Button>
+      Click Me
+    </Button>
   )
 }
