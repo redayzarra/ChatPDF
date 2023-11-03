48b33244ef84ed1fa49b9e43942488d8e77a3f16
ReDay Zarra <redayzarra@gmail.com>
2023-11-02 21:23:02 -0400

    Finished setting up Drizzle Studio

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+export default {
+  driver: "pg",
+  schema: "./lib/db/schema.ts",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
+
+// npx drizzle-kit push:pg
+// Go to localhost:#### instead of the link that DrizzleORM provides.
