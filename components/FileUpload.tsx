eaf3e21d3144dad651dcc11423e1eff5e468d167
ReDay Zarra <redayzarra@gmail.com>
2023-11-06 05:25:59 -0500

    Setting up upload functionality to S3 and adding to file upload

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,18 +1,70 @@
 "use client";
-
-import { Inbox } from "lucide-react";
+import { uploadToS3 } from "@/lib/s3";
+import { useMutation } from "@tanstack/react-query";
+import { Inbox, Loader2 } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+// https://github.com/aws/aws-sdk-js-v3/issues/4126
 
 const FileUpload = () => {
-  const { getRootProps, getInputProps } = useDropzone({
-    accept: { "application/pdf": [".pdf"] },
-    maxFiles: 1,
-    onDrop: (acceptedFile) => {
-      console.log(acceptedFile);
+  const router = useRouter();
+  const [uploading, setUploading] = React.useState(false);
+  const { mutate, status } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/create-chat", {
+        file_key,
+        file_name,
+      });
+      return response.data;
     },
   });
 
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "application/pdf": [".pdf"] },
+    maxFiles: 1,
+    onDrop: async (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        // bigger than 10mb!
+        toast.error("File cannot be larger than 10mb.");
+        return;
+      }
+
+      try {
+        setUploading(true);
+        const data = await uploadToS3(file);
+        console.log("meow", data);
+        if (!data?.file_key || !data.file_name) {
+          toast.error("Something went wrong");
+          return;
+        }
+        mutate(data, {
+          onSuccess: ({ chat_id }) => {
+            toast.success("Chat created!");
+            router.push(`/chat/${chat_id}`);
+          },
+          onError: (err) => {
+            toast.error("Error creating chat");
+            console.error(err);
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setUploading(false);
+      }
+    },
+  });
   return (
     <div className="p-2 bg-white rounded-xl">
       <div
@@ -22,11 +74,20 @@ const FileUpload = () => {
         })}
       >
         <input {...getInputProps()} />
-
-        <>
-          <Inbox className="w-10 h-10 text-blue-500" />
-          <p className="mt-2 text-sm text-slate-400">Drop PDF here</p>
-        </>
+        {uploading || status == "pending" ? (
+          <>
+            {/* loading state */}
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">
+              Analyzing with ChatGPT...
+            </p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+          </>
+        )}
       </div>
     </div>
   );
--- lib/s3.ts
+++ lib/s3.ts
@@ -1,13 +1,42 @@
-import AWS from "aws-sdk";
-import * as dotenv from "dotenv";
+import { PutObjectCommandOutput, S3 } from "@aws-sdk/client-s3";
 
-dotenv.config({ path: ".env" });
+export async function uploadToS3(
+  file: File
+): Promise<{ file_key: string; file_name: string }> {
+  return new Promise((resolve, reject) => {
+    try {
+      const s3 = new S3({
+        region: "us-east-2",
+        credentials: {
+          accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
+        },
+      });
 
-export async function uploadToS3(file: File) {
-  try {
-    AWS.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.AWS_SECRET_ACCESS_KEY,
-    });
-  } catch (error) {}
+      const file_key =
+        "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
+
+      const params = {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+        Key: file_key,
+        Body: file,
+      };
+      s3.putObject(
+        params,
+        (err: any, data: PutObjectCommandOutput | undefined) => {
+          return resolve({
+            file_key,
+            file_name: file.name,
+          });
+        }
+      );
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+export function getS3Url(file_key: string) {
+  const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.us-east-2.amazonaws.com/${file_key}`;
+  return url;
 }
